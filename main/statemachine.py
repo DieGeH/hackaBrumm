eb2f806b41620873b0e8df1833f4d6d979888f84
ariogato <ariodddddd@yahoo.de>
2018-11-17 20:01:50 +0100

    Veraenderung der Struktur der Statemachine \nonEnter und onExit

--- main/statemachine.py
+++ main/statemachine.py
@@ -21,6 +21,25 @@ class Statemachine:
         self.lightHandler = lightHandler.LightHandler()
         self.controller = controller.Controller()
 
+        # dictionaries mit den states als keys und calllables (Funktionen) als Werte
+        self.enterStateFunctions = {
+            States.init:            self.onEnterInit,
+            States.control:         self.onEnterControl,
+            States.ambientLight:    self.onEnterAmbientLight,
+            States.signalStandUp:   self.onEnterSignalStandUp,
+            States.signalDrink:     self.onEnterSignalDrink,
+            States.terminated:      self.onEnterTerminated
+        }
+
+        self.exitStateFunctions = {
+            States.init:            self.onExitInit(),
+            States.control:         self.onExitControl(),
+            States.ambientLight:    self.onExitAmbientLight,
+            States.signalStandUp:   self.onExitSignalStandUp,
+            States.signalDrink:     self.onExitSignalDrink,
+            States.terminated:      self.onExitTerminated
+        }
+
     def update(self):
         #   *************************************************       Initialisieren
         if self.current_state == States.init:
@@ -31,6 +50,9 @@ class Statemachine:
         #   *************************************************       AmbientLight
         elif self.current_state == States.ambientLight:
             self.ambientLight()
+        #   *************************************************       signalStandUp
+        elif self.current_state == States.signalStandUp:
+            self.signalStandUp()
 
 
 
@@ -38,18 +60,24 @@ class Statemachine:
 
     def init(self):
         self.lightHandler.initState()
-        self.current_state = States.ambientLight
+
+        #self.current_state = States.ambientLight
+        self.changeState(States.ambientLight)
 
         # Logging
         print("Initialization completed!")
 
     def control(self):
         signal = self.controller.evaluateSignal()
-        if(signal == 1):
-            self.current_state = States.signalStandUp
-        elif signal == 2:
-            self.current_state = States.signalDrink
+
         self.controller.calculateAmbientLight()
+
+        if(signal == 1):
+            #self.current_state = States.signalStandUp
+            self.changeState(States.signalStandUp)
+        elif signal == 2:
+            #self.current_state = States.signalDrink
+            self.changeState(States.signalDrink)
 
         # Logging
         print("control completed!")
@@ -61,9 +89,68 @@ class Statemachine:
         # Logging
         print("ambientLight completed!")
 
+
+    def signalStandUp(self):
+        lightHandler
+
     def isTerminated(self):
         return self.current_state == States.terminated
 
-    def changeToState(self, to):
+    def changeState(self, toState, fromState=-1):
+
+        if fromState == -1:
+            fromState = self.current_state
+
+        # Entsprechende onExit Funktion wird aufgerufen
+        self.exitStateFunctions[fromState]()
+
+        # Entsprechende onEnter Funktion wird aufgerufen
+        self.enterStateFunctions[toState]()
+
+
+
+    def onEnterInit(self):
         pass
 
+    def onExitInit(self):
+        pass
+
+
+
+    def onEnterControl(self):
+        pass
+
+    def onExitControl(self):
+        pass
+
+
+
+    def onEnterAmbientLight(self):
+        print("AmbientLight entered")
+
+    def onExitAmbientLight(self):
+        pass
+
+
+
+    def onEnterSignalStandUp(self):
+        pass
+
+    def onExitSignalStandUp(self):
+        pass
+
+
+
+    def onEnterSignalDrink(self):
+        pass
+
+    def onExitSignalDrink(self):
+        pass
+
+
+
+    def onEnterTerminated(self):
+        pass
+
+    def onExitTerminated(self):
+        pass
