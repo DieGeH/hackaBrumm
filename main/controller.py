e93aea5238c1705ec7a676a4b11692fde51f99f9
LENOVOT480R\fisch <fischesind@gmail.com>
2018-11-18 08:35:23 +0100

    blub

--- main/controller.py
+++ main/controller.py
@@ -1,33 +1,33 @@
 import time
-from datetime import datetime
+import datetime
+
 #   The purpose of the following code is to calculate the ideal value for our ambient light and to figure out if
 #   a signal needs to be sent
 
 class Controller:
     def __init__(self):
-        self.starttime = datetime.now()
-        self.lastStandUp = datetime.now()
-        self.lastDrink = datetime.now()
+        self.starttime = datetime.datetime.now()
+        self.lastStandUp = datetime.datetime.now()
+        self.lastDrink = datetime.datetime.now()
         self.runtime = 0
         self.daytime = 0
 
+        self.thresholdStandUp = None
+        self.thresholdDrink = None
+
     def evaluateSignal(self):
-        self.daytime = datetime.now()
+        self.daytime = datetime.datetime.now()
         diffInSecsStandup = int((self.daytime - self.lastStandUp).total_seconds())
-        # print(diffInSecs)
         diffInSecsDrink = int((self.daytime - self.lastDrink).total_seconds())
+
         if diffInSecsStandup == 10:
-            self.lastStandUp = datetime.now()
+            self.lastStandUp = datetime.datetime.now()
             print("stand up")
             return 1
-        if diffInSecsStandup == 30:
-            return -1
         if diffInSecsDrink == 150:
-            self.lastDrink = datetime.now()
+            self.lastDrink = datetime.datetime.now()
             print("drink")
             return 2
-        if diffInSecsDrink == 5:
-            return -2
         return 0
 
     def calculateAmbientLight(self):
@@ -35,13 +35,28 @@ class Controller:
         red = 255
         green = 255
         blue = 255
-        self.daytime = datetime.now()
+        self.daytime = datetime.datetime.now()
         workdurationSecs = (self.daytime-self.starttime).total_seconds()
-        if 50 <= workdurationSecs <= (50 + 60):
-            blue = int(-(55/(60)) * workdurationSecs + 255)
         ambientRGB = (red, green, blue)
         ambientBrightness = 255
         return [ambientRGB, ambientBrightness]
 
     def resetStartTime(self):
-        self.starttime = datetime.now()
+        self.starttime = datetime.datetime.now()
+
+
+    def setThresholdStandUp(self, duration):
+        #Input: duration in seconds
+        self.thresholdStandUp = datetime.datetime.now() + datetime.timedelta(0, duration)
+
+
+    def setThresholdDrink(self, duration):
+        #Input: duration in seconds
+        self.thresholdDrink = datetime.datetime.now() + datetime.timedelta(0, duration)
+
+
+    def isTerminateStandUp(self):
+        return datetime.datetime.now() >= self.thresholdStandUp
+
+    def isTerminateDrink(self):
+        return datetime.datetime.now() >= self.thresholdDrink
